77ed67178ccca20c05be6004a42ff74edd8284c6
BingusShmingus <156937486+BingusShmingus@users.noreply.github.com>
2024-06-13 21:50:02 -0500

    Update h1.js

--- h1.js
+++ h1.js
@@ -1,11 +1,1 @@
-const sentences = [
-  "Welcome to My Website!",
-  "Oi Mate Welcome to My Site!",
-  "Rivers Quomo."
-];
-
-const randomIndex = Math.floor(Math.random() * sentences.length);
-const randomSentence = sentences[randomIndex];
-
-const sentenceElement = document.getElementById("sentence");
-sentenceElement.textContent = randomSentence;
+//nothing here either
