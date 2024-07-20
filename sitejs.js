2bb5016b1f75748f701803de09d33fe6da3d1045
Hunter N. <156937486+BingusShmingus@users.noreply.github.com>
2024-07-19 22:32:49 -0500

    Update sitejs.js

--- sitejs.js
+++ sitejs.js
@@ -1,14 +1,11 @@
 const sentences = [
-  "What even is a website?",
-  "Made with JavaScript.",
-  "Made with HTML.",
-  "Made with CSS.",
-  "Made with Love",
-  "You know I can see you, right?.",
-  "My favorite game is Halo Reach!",
-  "Goose is a huge dork,",
-  "Check out my HUGE rack.",
-  "Frank the Snake.",
+"It's one of life's great mysteries, isn't it?... I don't know, man, but it keeps me up at night.",
+"I think it looks more like a puma.",
+"Don't. Ever. Be. Alone.",
+"We need you to go to the store and get two quarts of elbow grease.",
+"Thank you for calling Red Base, this is Private Grif, how may I assist you today?",
+"Time is not made out of lines. It is made out of circles; that is why clocks are round.",
+"You ever wonder why we're here?"
 ];
 
 const randomIndex = Math.floor(Math.random() * sentences.length);
